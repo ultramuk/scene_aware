6f26c0b43325d22f14b1bb4029ca5f054c89fb8c
ultramuk <koungmuk1975@gmail.com>
2025-06-11 10:51:30 +0900

    Chore: test 모듈이 inference libraries를 읽지 못하므로 PRIVATE이 아니라 PUBLIC으로 변경

--- cmake/module/common/add_target_dependencies.cmake
+++ cmake/module/common/add_target_dependencies.cmake
@@ -2,6 +2,6 @@ function(add_target_dependencies TARGET_NAME)
     cmake_parse_arguments(ARGUMENTS "" "" "DEPENDENCIES" ${ARGN})
 
     if(ARGUMENTS_DEPENDENCIES)
-        target_link_libraries(${TARGET_NAME} PRIVATE ${ARGUMENTS_DEPENDENCIES})
+        target_link_libraries(${TARGET_NAME} PUBLIC ${ARGUMENTS_DEPENDENCIES})
     endif()
 endfunction()
