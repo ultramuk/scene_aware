fc3797d461f88e8ba95740ffe2322bd814dd73df
ultramuk <koungmuk1975@gmail.com>
2025-06-12 13:50:39 +0900

    Fix: 오타 수정

--- libraries/inference/include/inference/core/config.hpp
+++ libraries/inference/include/inference/core/config.hpp
@@ -2,9 +2,9 @@
 
 namespace inference::core {
 
-struct InterConfig {
+struct InferConfig {
     int input_width = 640;
-    int intput_height = 640;
+    int input_height = 640;
     float conf_threshold = 0.5f;
     float nms_threshold = 0.45f;
     bool use_fp16 = true;
