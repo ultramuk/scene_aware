29b917fb8bad81f261f6c01acce2a8ba62764aba
ultramuk <koungmuk1975@gmail.com>
2025-07-21 15:52:35 +0900

    Feat: Add graphviz installation to gcovr setup script

--- scripts/install/os/ubuntu/install_gcovr.sh
+++ scripts/install/os/ubuntu/install_gcovr.sh
@@ -7,7 +7,10 @@ sudo apt upgrade -y
 # Install gcovr
 echo "Installing gcovr..."
 sudo apt install -y gcovr
+sudo apt install -y graphviz
 
 # Verify installed version
 echo "gcovr version:"
 gcovr --version
+echo "graphviz version:"
+graphviz --version
