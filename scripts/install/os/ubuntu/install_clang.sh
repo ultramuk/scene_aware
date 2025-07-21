43d88df4ea525b0490b9f60ac04d3a96e24ae583
ultramuk <koungmuk1975@gmail.com>
2025-07-21 15:52:01 +0900

    Fix: clang_tidy, clang_format 추가

--- scripts/install/os/ubuntu/install_clang.sh
+++ scripts/install/os/ubuntu/install_clang.sh
@@ -4,10 +4,17 @@
 sudo apt update -y
 sudo apt upgrade -y
 
+# Install clang-format
+echo "Installing clang-format..."
+sudo apt install -y clang-format
+
 # Install clang-tidy
 echo "Installing clang-tidy..."
 sudo apt install -y clang-tidy
 
 # Verify installed version
+echo "clang-format version:"
+clang-format --version
+
 echo "clang-tidy version:"
 clang-tidy --version
